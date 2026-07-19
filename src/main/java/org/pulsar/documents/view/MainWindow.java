4a96e15f9d4c82cc18186ac25a627ef946ebf445
pulsar <pulsarmn@yandex.ru>
2026-07-19 15:26:57 +0300

    feat: add a central panel on main window

--- src/main/java/org/pulsar/documents/view/MainWindow.java
+++ src/main/java/org/pulsar/documents/view/MainWindow.java
@@ -1,8 +1,18 @@
 package org.pulsar.documents.view;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.control.ListView;
 import javafx.scene.layout.BorderPane;
+import org.pulsar.documents.model.Document;
 
 
 public class MainWindow extends BorderPane {
 
+    private final ObservableList<Document> documents = FXCollections.observableArrayList();
+    private final ListView<Document> listView = new ListView<>(documents);
+
+    public MainWindow() {
+        super.setCenter(listView);
+    }
 }
