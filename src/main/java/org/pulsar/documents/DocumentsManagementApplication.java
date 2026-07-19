4962faf4baaf4fa4447fc113732e94479cb6a8a6
pulsar <pulsarmn@yandex.ru>
2026-07-19 15:43:51 +0300

    feat: add main window to the primary stage

--- src/main/java/org/pulsar/documents/DocumentsManagementApplication.java
+++ src/main/java/org/pulsar/documents/DocumentsManagementApplication.java
@@ -1,13 +1,18 @@
 package org.pulsar.documents;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.pulsar.documents.view.MainWindow;
 
 
 public class DocumentsManagementApplication extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
+        MainWindow mainWindow = new MainWindow();
+        Scene scene = new Scene(mainWindow, 600, 400);
+        stage.setScene(scene);
         stage.setTitle("Тест");
         stage.show();
     }
