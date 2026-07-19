04f3b4b13499fd74cf4176b9a14b9f83fcee0bd4
pulsar <pulsarmn@yandex.ru>
2026-07-19 14:34:17 +0300

    feat: add application launcher

---
+++ src/main/java/org/pulsar/documents/DocumentsManagementApplication.java
@@ -0,0 +1,14 @@
+package org.pulsar.documents;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+
+public class DocumentsManagementApplication extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setTitle("Тест");
+        stage.show();
+    }
+}
---
+++ src/main/java/org/pulsar/documents/Launcher.java
@@ -0,0 +1,11 @@
+package org.pulsar.documents;
+
+
+import javafx.application.Application;
+
+public class Launcher {
+
+    static void main(String[] args) {
+        Application.launch(DocumentsManagementApplication.class, args);
+    }
+}
