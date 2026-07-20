cdfbfb6a7c028ca3293e74ace20b3c154cb850d1
pulsar <pulsarmn@yandex.ru>
2026-07-20 10:41:24 +0300

    feat: create the frame of the invoice dialog window

---
+++ src/main/java/org/pulsar/documents/view/dialog/InvoiceDialog.java
@@ -0,0 +1,132 @@
+package org.pulsar.documents.view.dialog;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import org.pulsar.documents.model.Document;
+
+
+public class InvoiceDialog extends Stage {
+
+    private final ObservableList<Document> documents;
+
+    private TextField numberField;
+    private DatePicker datePicker;
+    private TextField userField;
+    private TextField sumField;
+    private ComboBox<String> currencyField;
+    private TextField currencyRateField;
+    private TextField productField;
+    private TextField countField;
+
+    public InvoiceDialog(ObservableList<Document> documents) {
+        this.documents = documents;
+        initModality(Modality.APPLICATION_MODAL);
+        GridPane gridPane = createDialogContent();
+        setScene(new Scene(gridPane, 400, 600));
+    }
+
+    private GridPane createDialogContent() {
+        GridPane gridPane = new GridPane(10, 15);
+        gridPane.setPadding(new Insets(10));
+        gridPane.setAlignment(Pos.TOP_CENTER);
+
+        addNumberField(gridPane);
+        addDatePicker(gridPane);
+        addUserField(gridPane);
+        addSumField(gridPane);
+        addCurrencyField(gridPane);
+        addCurrencyRateField(gridPane);
+        addProductField(gridPane);
+        addCountField(gridPane);
+        addSaveButton(gridPane);
+
+        return gridPane;
+    }
+
+    private void addNumberField(GridPane gridPane) {
+        Label numberLabel = new Label("Номер:");
+        numberField = new TextField();
+        numberField.setPromptText("123");
+
+        gridPane.add(numberLabel, 0, 0);
+        gridPane.add(numberField, 1, 0);
+    }
+
+    private void addDatePicker(GridPane gridPane) {
+        Label dateLabel = new Label("Дата:");
+        datePicker = new DatePicker();
+
+        gridPane.add(dateLabel, 0, 1);
+        gridPane.add(datePicker, 1, 1);
+    }
+
+    private void addUserField(GridPane gridPane) {
+        Label userLabel = new Label("Пользователь:");
+        userField = new TextField();
+        userField.setPromptText("Александр");
+
+        gridPane.add(userLabel, 0,  2);
+        gridPane.add(userField, 1, 2);
+    }
+
+    private void addSumField(GridPane gridPane) {
+        Label sumLabel = new Label("Сумма:");
+        sumField = new TextField();
+        sumField.setPromptText("0");
+
+        gridPane.add(sumLabel, 0, 3);
+        gridPane.add(sumField, 1, 3);
+    }
+
+    private void addCurrencyField(GridPane gridPane) {
+        Label currencyLabel = new Label("Валюта:");
+        currencyField = new ComboBox<>();
+        currencyField.setItems(getCurrencies());
+
+        gridPane.add(currencyLabel, 0, 4);
+        gridPane.add(currencyField, 1, 4);
+    }
+
+    private ObservableList<String> getCurrencies() {
+        return FXCollections.observableArrayList("Рубль", "Доллар", "Евро");
+    }
+
+    private void addCurrencyRateField(GridPane gridPane) {
+        Label currencyRateLabel = new Label("Курс валюты:");
+        currencyRateField = new TextField();
+        currencyRateField.setPromptText("0");
+
+        gridPane.add(currencyRateLabel, 0, 5);
+        gridPane.add(currencyRateField, 1, 5);
+    }
+
+    private void addProductField(GridPane gridPane) {
+        Label productLabel = new Label("Товар:");
+        productField = new TextField();
+        productField.setPromptText("Товар");
+
+        gridPane.add(productLabel, 0, 6);
+        gridPane.add(productField, 1, 6);
+    }
+
+    private void addCountField(GridPane gridPane) {
+        Label countLabel = new Label("Количество:");
+        countField = new TextField();
+        countField.setPromptText("0");
+
+        gridPane.add(countLabel, 0, 7);
+        gridPane.add(countField, 1, 7);
+    }
+
+    private void addSaveButton(GridPane gridPane) {
+        Button saveButton = new Button("OK");
+        gridPane.add(saveButton, 1, 8);
+    }
+}
